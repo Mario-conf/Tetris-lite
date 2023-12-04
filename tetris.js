6ced9129606d0359397fc7b6125ea3fb0c567749
Mario AV <152533010+Mario-conf@users.noreply.github.com>
2023-12-04 21:49:16 +0100

    Create tetris.js

---
+++ tetris.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const canvas = document.getElementById('tetrisCanvas');
+    const context = canvas.getContext('2d');
+
+    const ROWS = 20;
+    const COLUMNS = 10;
+    const BLOCK_SIZE = 30;
+    const EMPTY = 'white';
+
+    let board = [];
+
+  
+    for (let row = 0; row < ROWS; row++) {
+        board[row] = [];
+        for (let col = 0; col < COLUMNS; col++) {
+            board[row][col] = EMPTY;
+        }
+    }
+
+
+    function drawSquare(x, y, color) {
+        context.fillStyle = color;
+        context.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+        context.strokeStyle = 'black';
+        context.strokeRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    }
+
+  
+    function drawBoard() {
+        for (let row = 0; row < ROWS; row++) {
+            for (let col = 0; col < COLUMNS; col++) {
+                drawSquare(col, row, board[row][col]);
+            }
+        }
+    }
+
+    drawBoard();
+});
